d0f35dc803cf96ffcaec108cf2daf0db824ffc71
nathanaelraj <nathanae001@e.ntu.edu.sg>
2016-10-09 12:20:58 +0800

    updated
    updated

--- ALGO LAB 3-Sorting/src/Sorting_Algorithms.java
+++ ALGO LAB 3-Sorting/src/Sorting_Algorithms.java
@@ -10,7 +10,7 @@ public class Sorting_Algorithms {
 		}
 	}
 	
-	public static int[] arrayGen(int n){
+	public static int[] randArrayGen(int n){
 		int i;
 		int[] array = new int[n];
 		for(i = 0; i < n; i++){
@@ -18,17 +18,37 @@ public class Sorting_Algorithms {
 		}	
 		return array;
 	}
-	
-	public static void quickSortAsc(int s, int e, int[] ar){
-		if (e-s <= 0) return;
-		int piv = partitionAsc(s, e, ar);
-		quickSortAsc(s, (piv-1), ar);
-		quickSortAsc((piv+1), e, ar);
+	public static int[] ascArrayGen(int n){
+		int i;
+		int[] array = new int[n];
+		for(i = 0; i < n; i++){
+			array[i] = i;	
+		}	
+		return array;
 	}
-	public static int partitionAsc(int s, int e, int[] ar){
+	public static int[] dscArrayGen(int n){
+		int i;
+		int[] array = new int[n];
+		for(i = 0; i < n; i++){
+			array[i] = (n - i);	
+		}	
+		return array;
+	}
+	
+	public static void quickSort(int s, int e, int[] ar){
+		if (e-s <= 0) return;
+		int piv = partition(s, e, ar);
+		quickSort(s, (piv-1), ar);
+		quickSort((piv+1), e, ar);
+	}
+	public static int partition(int s, int e, int[] ar){
 		int last_small = s;
 		int tmp;
 		int pivot = ar[s]; 
+		int mid = (s+e)/2;
+		tmp = ar[mid];
+		ar[mid] = ar[s];
+		ar[s] = tmp;
 		for (int i = s+1; i<=e; i++){
 			if (ar[i] < pivot){
 				tmp = ar[++last_small];
@@ -44,43 +64,19 @@ public class Sorting_Algorithms {
 		return last_small;		
 	}
 	
-	public static void quickSortDsc(int s, int e, int[] ar){
-		if (e-s <= 0) return;
-		int piv = partitionDsc(s, e, ar);
-		quickSortDsc(s, (piv-1), ar);
-		quickSortDsc((piv+1), e, ar);
-	}
-	public static int partitionDsc(int s, int e, int[] ar){
-		int last_small = s;
-		int tmp;
-		int pivot = ar[s]; 
-		for (int i = s+1; i<=e; i++){
-			if (ar[i] > pivot){
-				tmp = ar[++last_small];
-				ar[last_small] = ar[i];
-				ar[i] = tmp;
-				
-			}
-			count++;
-		}
-		tmp = ar[last_small];
-		ar[last_small] = ar[s];
-		ar[s] = tmp;
-		return last_small;		
-	}
 	
-	public static void MergeSortAsc(int s, int e, int[] ar){
+	public static void mergeSort(int s, int e, int[] ar){
 		if (s == e) return;
 		else{
 			int mid = (s+e)/2;
-			MergeSortAsc(s, mid, ar);
-			MergeSortAsc(mid+1, e, ar);
-			MergeAsc(s, e, ar);
+			mergeSort(s, mid, ar);
+			mergeSort(mid+1, e, ar);
+			merge(s, e, ar);
 			return;
 		}
 		
 	}
-	public static void MergeAsc(int s, int e, int[] ar){
+	public static void merge(int s, int e, int[] ar){
 		if ((e - s)<= 1){
 			if (ar[s] <= ar[e]){
 				count++;
@@ -125,112 +121,60 @@ public class Sorting_Algorithms {
 			return;
 		}		
 	}
-	
-	public static void MergeSortDsc(int s, int e, int[] ar){
-		if (s == e) return;
-		else{
-			int mid = (s+e)/2;
-			MergeSortDsc(s, mid, ar);
-			MergeSortDsc(mid+1, e, ar);
-			MergeDcs(s, e, ar);
-			return;
-		}
-		
-	}
-	public static void MergeDcs(int s, int e, int[] ar){
-		if ((e - s)<= 1){
-			if (ar[s] >= ar[e]){
-				count++;
-				return;
-			}
-			
-			else{
-				count++;
-				int tmp;
-				tmp = ar[e];
-				ar[e] = ar[s];
-				ar[s] = tmp;
-				return;
-			}
-		}
-		else {
-			int mid = (s+e)/2;
-			int [] temp = new int[ar.length];
-			int a, b, i;
-			for (i = 0; i < ar.length; i++){
-				temp[i] = ar[i];				
-			}
-			a = s;
-			b = mid + 1;
-			for (i = s; i <= e; i++){
-				if (temp[a] >= temp[b]){
-					ar[i] = temp[a++];
-					count++;
-				}
-				else {
-					count++;
-					ar[i] = temp[b++];
-				}
-				if(a > mid) break;
-				if(b > e){
-					while (i < e){
-						ar[++i] = temp[a++];					
-					}
-					break;
-				}			
-			}
-	
-			return;
-		}		
-	}
 
 	public static void sorting(int n) {
-		int[] toSort = arrayGen(n);
-		int[] toMergeDsc = new int[n];
-		int[] toMergeAsc = new int[n];
-		int[] toQuickDsc = new int[n];
-		int[] toQuickAsc = new int[n];
-		for (int i = 0; i<n; i++){
-			toMergeDsc[i] = toSort[i];			
-		}
-		for (int i = 0; i<n; i++){
-			toMergeAsc[i] = toSort[i];			
-		}
-		for (int i = 0; i<n; i++){
-			toQuickDsc[i] = toSort[i];			
-		}
-		for (int i = 0; i<n; i++){
-			toQuickAsc[i] = toSort[i];			
-		}
+		int[] randQuick = randArrayGen(n);
+		int[] ascQuick = ascArrayGen(n);
+		int[] dscQuick = dscArrayGen(n);
+		int[] randMerge = randArrayGen(n);
+		int[] ascMerge = ascArrayGen(n);
+		int[] dscMerge = dscArrayGen(n);
+		int[] test = dscArrayGen(n);
 		
 		count = 0;
 		long starttime = System.nanoTime();
-		MergeSortDsc(0, n-1, toMergeDsc);
+		mergeSort(0, n-1, randMerge);
 		long endtime = System.nanoTime();
-		System.out.println("Descending MergeSort runtime for size = " + n +": "+((endtime - starttime) +" nanoseconds"));
-		System.out.println("No. of comparisons made: " + count);
+		System.out.println("MergeSorting random array of size = " + n +" takes: "+((endtime - starttime) +" nanoseconds"));
+		System.out.println("Comparisons: " + count);
 		
 		count = 0;
 		starttime = System.nanoTime();
-		MergeSortAsc(0, n-1, toMergeAsc);
+		mergeSort(0, n-1, ascMerge);
 		endtime = System.nanoTime();
-		System.out.println("Ascending MergeSort runtime for size = " + n +": "+((endtime - starttime) +" nanoseconds"));
-		System.out.println("No. of comparisons made: " + count);
+		System.out.println("MergeSorting ascending array of size = " + n +" takes: "+((endtime - starttime) +" nanoseconds"));
+		System.out.println("Comparisons: " + count);
+		
+		
+		count = 0;
+		starttime = System.nanoTime();
+		mergeSort(0, n-1, dscMerge);
+		endtime = System.nanoTime();
+		System.out.println("MergeSorting descending array of size = " + n +" takes: "+((endtime - starttime) +" nanoseconds"));
+		System.out.println("Comparisons: " + count);
 		System.out.println();
 		
 		count = 0;
 		starttime = System.nanoTime();
-		quickSortDsc(0, n-1, toQuickDsc);
+		quickSort(0, n-1, randQuick);
 		endtime = System.nanoTime();
-		System.out.println("Descending QuickSort runtime for size = " + n +": "+((endtime - starttime) +" nanoseconds"));
-		System.out.println("No. of comparisons made: " + count);
+		System.out.println("QuickSorting random array of size = " + n +" takes: "+((endtime - starttime) +" nanoseconds"));
+		System.out.println("Comparisons: " + count);
 		
 		count = 0;
 		starttime = System.nanoTime();
-		quickSortAsc(0, n-1, toQuickAsc);
+		quickSort(0, n-1, ascQuick);
 		endtime = System.nanoTime();
-		System.out.println("Ascending QuickSort runtime for size = " + n +": "+((endtime - starttime) +" nanoseconds"));
-		System.out.println("No. of comparisons made: " + count);
+		System.out.println("QuickSorting ascending array size = " + n +" takes: "+((endtime - starttime) +" nanoseconds"));
+		System.out.println("Comparisons: " + count);
+		
+		
+		count = 0;
+		starttime = System.nanoTime();
+		quickSort(0, n-1, dscQuick);
+		endtime = System.nanoTime();
+		System.out.println("QuickSorting descending array of size = " + n +" takes: "+((endtime - starttime) +" nanoseconds"));
+		System.out.println("Comparisons: " + count);
 		System.out.println();
 		
 		try{
@@ -239,34 +183,47 @@ public class Sorting_Algorithms {
 		    out.write("Unordered array:" );
 		    out.newLine();
 		    for(int k = 0; k < n; k++){
-		        out.write(String.valueOf(toSort[k]) +" ");
+		        out.write(String.valueOf(randMerge[k]) +" ");
 		    }
 		    out.newLine();
-		    out.write("Merged Sorted in Ascending order:");
+		    out.write("Random array MergeSorted:");
 		    out.newLine();
 		    for(int k = 0; k < n; k++){
-		        out.write(String.valueOf(toMergeAsc[k]) +" ");
+		        out.write(String.valueOf(randMerge[k]) +" ");
 		    }
 		    out.newLine();
-		    out.write("Merge Sorted in Descending order:\n");
+		    out.write("Ascending array MergeSorted:");
 		    out.newLine();
 		    for(int k = 0; k < n; k++){
-		        out.write(String.valueOf(toMergeDsc[k]) +" ");
+		        out.write(String.valueOf(ascMerge[k]) +" ");
 		    }
 		    out.newLine();
-		    out.write("Quick Sorted in Ascending order:");
+		    out.write("Descending array MergeSorted:");
 		    out.newLine();
 		    for(int k = 0; k < n; k++){
-		        out.write(String.valueOf(toQuickAsc[k]) +" ");
+		        out.write(String.valueOf(dscMerge[k]) +" ");
 		    }
 		    out.newLine();
-		    out.write("Quick Sorted in Descending order:\n");
+		    out.write("Random array QuickSorted:");
 		    out.newLine();
 		    for(int k = 0; k < n; k++){
-		        out.write(String.valueOf(toQuickDsc[k]) +" ");
+		        out.write(String.valueOf(randQuick[k]) +" ");
 		    }
+		    out.newLine();
+		    out.write("Ascending array QuickSorted:");
+		    out.newLine();
+		    for(int k = 0; k < n; k++){
+		        out.write(String.valueOf(ascQuick[k]) +" ");
+		    }
+		    out.newLine();
+		    out.write("Descending array QuickSorted:");
+		    out.newLine();
+		    for(int k = 0; k < n; k++){
+		        out.write(String.valueOf(dscQuick[k]) +" ");
+		    }
+		    
 		    out.close();
-		    }catch (Exception e){//Catch exception if any
+		    }catch (Exception e){
 		      System.err.println("Error: " + e.getMessage());
 		    }
 	}
